3bfb2f1d566546a52bfd69f1b8439ff1ef386f4b
Dennis Bovermann <dennisbovermann@gmail.com>
2025-02-22 18:05:59 +0100

    login route

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,4 +1,26 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user';
+import type { HttpContext } from '@adonisjs/core/http'
+import hash from '@adonisjs/core/services/hash';
 
 export default class AuthController {
+
+    async issueToken({ request, response, auth }: HttpContext) {
+        const { email, password } = request.all()
+        const user = await User.findBy('email', email);
+
+        if (user === null) {
+            return response.notFound('user_not_found')
+        }
+
+        if (await hash.verify(user.password, password)) {
+            const token = await User.accessTokens.create(user)
+
+            response.cookie('api', token.value!.release(), {
+                httpOnly: true
+            })
+            return response.ok(user)
+        }
+
+        return response.badGateway()
+    }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -7,4 +7,10 @@
 |
 */
 
+import AuthController from '#controllers/auth_controller'
 import router from '@adonisjs/core/services/router'
+
+/*
+###### AUTHENTICATION ######
+*/
+router.get('api/v1/auth/login', [AuthController, 'issueToken'])
